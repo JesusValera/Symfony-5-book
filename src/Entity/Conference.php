847d0d0a43aace1f17cbdd9e571d4e02b8566a0e
Jesús Valera Reales <jesus1902@outlook.com>
2020-04-10 19:44:12 +0200

    Add EasyAdmin bundle

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -43,6 +43,11 @@ class Conference
         $this->comments = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->city . ' ' . $this->year;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
