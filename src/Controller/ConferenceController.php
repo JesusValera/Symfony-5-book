25be046ef3f6ee6483faea64b7fbc316916fd71c
Jesús Valera Reales <jesus1902@outlook.com>
2020-04-10 20:51:30 +0200

    Inject dependencies into ConferenceController:index() and replace response

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,31 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 final class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-
-        return new Response(<<<EOF
-<HTML>
-  <body>
-    <img src="/images/under-construction.gif">
-    $greet
-  </body>
-</HTML>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
