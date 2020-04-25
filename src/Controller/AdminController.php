587a5fc0686118958a867b0ce5b036d29c1e0740
Jesús Valera Reales <jesus1902@outlook.com>
2020-04-25 17:44:25 +0200

    Purging the HTTP cache for Testing

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,13 +8,18 @@ use App\Entity\Comment;
 use App\Message\CommentMessage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Workflow\Registry;
 use Twig\Environment;
 
+/**
+ * @Route("/admin")
+ */
 final class AdminController extends AbstractController
 {
     private Environment $twig;
@@ -32,7 +37,7 @@ final class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/comment/review/{id}", name="review_comment")
+     * @Route("/comment/review/{id}", name="review_comment")
      */
     public function reviewComment(Request $request, Comment $comment, Registry $registry)
     {
@@ -58,5 +63,20 @@ final class AdminController extends AbstractController
             'transition' => $transition,
             'comment' => $comment,
         ]);
+    }
+
+    /**
+     * @Route("/http-cache/{uri<.*>}", methods={"PURGE"})
+     */
+    public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri): Response
+    {
+        if ('prod' === $kernel->getEnvironment()) {
+            return new Response('KO', 400);
+        }
+
+        $store = (new class($kernel) extends HttpCache {})->getStore();
+        $store->purge($request->getSchemeAndHttpHost() . '/' . $uri);
+
+        return new Response('Done');
     }
 }
