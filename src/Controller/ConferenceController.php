76ce0cc944e6e678e9ea4df10505e8ccd67a5211
Jesús Valera Reales <jesus1902@outlook.com>
2020-05-05 23:58:04 +0200

    Add locale to ConferenceController routes

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -40,7 +40,15 @@ final class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/")
+     */
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'en']);
+    }
+
+    /**
+     * @Route("/{_locale<%app.supported_locales%>}/", name="homepage")
      */
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -53,7 +61,7 @@ final class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/conference_header", name="conference_header")
+     * @Route("/{_locale<%app.supported_locales%>}/conference_header", name="conference_header")
      */
     public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
     {
@@ -66,7 +74,7 @@ final class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/conference/{slug}", name="conference")
+     * @Route("/{_locale<%app.supported_locales%>}/conference/{slug}", name="conference")
      */
     public function show(
         Request $request,
